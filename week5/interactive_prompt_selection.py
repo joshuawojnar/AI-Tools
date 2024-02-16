8c90ba34e104149924e466d40d28d3014541a33d
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-02-15 20:41:47 -0700

    edit variable name

--- week5/interactive_prompt_selection.py
+++ week5/interactive_prompt_selection.py
@@ -1,19 +1,19 @@
 from utilities import ChatTemplate
 import json
 
-selection = ChatTemplate.from_file('selection_chat.json')
+selectionTemplate = ChatTemplate.from_file('selection_chat.json')
 
 while True:
     ask = input('ask: ')
     if ask == 'exit':
         break
     
-    response = selection.completion({'ask': ask})
+    response = selectionTemplate.completion({'ask': ask})
     print(response.choices[0].message.content)
-    
-    content = json.loads(response.choices[0].message.content)
 
-    response = ChatTemplate.from_file(content['as'] + '_chat.json').completion({
-        'action': content['action'],
-        'document': content['document']})
+    selection = json.loads(response.choices[0].message.content)
+
+    response = ChatTemplate.from_file(selection['as'] + '_chat.json').completion({
+        'action': selection['action'],
+        'document': selection['document']})
     print(response.choices[0].message.content)
