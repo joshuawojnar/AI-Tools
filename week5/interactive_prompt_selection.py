ea2e8bcc528b4c096f8ae6c51824e1db136fab75
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-02-19 14:44:50 -0700

    added comments

--- week5/interactive_prompt_selection.py
+++ week5/interactive_prompt_selection.py
@@ -7,16 +7,31 @@ import json
 selectionTemplate = ChatTemplate.from_file('selection_chat.json')
 
 while True:
+    # our example: "please write a poem about ASL"
     ask = input('ask: ')
     if ask == 'exit':
         break
     
+    # first LLM call. This time, AI returned a json file which looks like:
+    #   {"as": "writer", "action": "compose", "document": "a poem about American Sign Language."}
     response = selectionTemplate.completion({'ask': ask})
     print(response.choices[0].message.content)
 
+    # load the json file to memory (basically translate the json file to a python object)
     selection = json.loads(response.choices[0].message.content)
 
+    '''
+    `selection` object:
+    - as: writer
+    - action: compose
+    - document: a poem about american sign language
+    '''
+    # making the second LLM call. 
+    #   here, we need to tell the AI to look for the right template, which is `writer_chat.json`.
+    #   note that `selection['as']` is `writer` in this case, 
+    #   thus selection['as'] + '_chat.json' -> writer_chat.json
     response = ChatTemplate.from_file(selection['as'] + '_chat.json').completion({
         'action': selection['action'],
         'document': selection['document']})
+    
     print(response.choices[0].message.content)
