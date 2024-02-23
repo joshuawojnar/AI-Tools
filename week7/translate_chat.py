edcd581a547d8c148c31811fcfd871f3951989a3
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-02-23 12:59:17 -0800

    add comment

--- week7/translate_chat.py
+++ week7/translate_chat.py
@@ -3,6 +3,8 @@ sys.path.append('../')
 
 from utilities import ChatTemplate
 
+#.from_file(prompt template)
+#.completion(user input prompt)
 response = ChatTemplate.from_file(
     'translate.json').completion({'text': "Live long and prosper!"})
 
