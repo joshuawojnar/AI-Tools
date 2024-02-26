a90889f2c6ea83c4ea2b4dc3e94ccc773e389f2b
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-02-26 13:01:15 -0800

    fix bug

--- week8/data_prep.py
+++ week8/data_prep.py
@@ -6,13 +6,13 @@ import os
 
 # for honmework 6, please change both values below based on the hw instructions
 folder_name = '../signlanguages'
-output_file = 'signlanguages.jsonl'
+output_file = 'signlanguages_output.jsonl'
 
 template = ChatTemplate.from_file('jsonl.json')
 jsonl = ''
 
 for f in os.listdir(folder_name):
-    with open(os.path.join(folder_name, f), 'r') as f:
+    with open(os.path.join(folder_name, f), 'r', encoding="utf-8", errors="ignore") as f:
         text = f.read()
 
     response = template.completion({'info': text, 'n': '20'})
